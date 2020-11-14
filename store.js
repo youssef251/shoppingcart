0efac1f3c7c1547420fe951647a5306f79ae4163
youssef251 <70808096+youssef251@users.noreply.github.com>
2020-11-14 14:31:05 +0100

    Add files via upload

--- store.js
+++ store.js
@@ -1,3 +1,9 @@
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+
 function ready() {
     var removeCartItemButtons = document.getElementsByClassName('btn-danger')
     for (var i = 0; i < removeCartItemButtons.length; i++) {
